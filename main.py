71d875a21b5f815092239c51a227c05e46c62d98
Jean Dubois <jd-dev@laposte.net>
2021-11-02 19:11:00 +0100

    oops, I forgot to code this before committing X)

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # coding:utf-8
-
+# INFOS FOR TESTING :
+# UUID of jddev : f32958ee-848f-4f90-a830-99446a88afab
+# flagindex of jddev : 51773
+# coordinates of jddev : 136:159
+# color of jddev : #10EDB8
 import requests
 
 mode = input("Mode :\n"
              "Récupérer le nom d'un pixel depuis son UUID (entrer r)\n"
              "Récupérer la liste de tous les pixels (entrer l)\n"
-             "Entrer choix : ")
+             "Entrer choix : ")  # on récup le mode
 
-if mode == 'r':
+if mode == 'r':  # on récupère depuis l'UUID
     uuid = input("Entrez UUID : ")
     user_data = requests.get(f"https://admin.fouloscopie.com/users/{uuid}").json()
 
-    {
+    data = {
             'flag_index': user_data['indexInFlag'],
             'color': user_data['hexColor'],
             'name': user_data['data']['last_name']
-        }
-    print(requests.get(f"https://admin.fouloscopie.com/users/{}").json()['data'])
+            }
+    print(requests.get(f"https://admin.fouloscopie.com/users/{data}").json()['data'])
 elif mode == 'l':
     flagRequest = requests.get('https://api-flag.fouloscopie.com/flag')
     print(flagRequest.headers['content-type'])
