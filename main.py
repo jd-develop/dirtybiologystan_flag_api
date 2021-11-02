e9bf572457a48cb53e51d2bf428c9637a2878e48
Jean Dubois <jd-dev@laposte.net>
2021-11-02 15:40:24 +0100

    add color

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ flagRequest = requests.get('https://api-flag.fouloscopie.com/flag')
 print(flagRequest.headers['content-type'])
 
 user_raw_list = flagRequest.json()
-cityzen_list: list[dict] = []  # dict('name', 'flag_index')
+cityzen_list: list[dict] = []  # dict('flag_index', 'color', 'name')
 
 for user in user_raw_list:
     user_data = requests.get(f"https://admin.fouloscopie.com/users/{user['author']}").json()['data']
-    cityzen_list.append({'name': user_data['last_name'], 'flag_index': user['indexInFlag']})
+    cityzen_list.append(
+        {
+            'flag_index': user['indexInFlag'],
+            'color': user['hexColor'],
+            'name': user_data['last_name']
+        }
+    )
     print(cityzen_list[len(cityzen_list) - 1])
